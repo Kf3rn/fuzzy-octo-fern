5caeb9b632609e8a1dad5aa2c32b23e895e71375
Kristine Fernadez <83658114+Kf3rn@users.noreply.github.com>
2024-03-14 00:24:48 +0800

    Update T-clock update.cpp

--- T-clock update.cpp
+++ T-clock update.cpp
@@ -1,48 +1,48 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <DS1302.h>
+#include <Wire.h> // Inilagay ang Wire library para sa I2C communication
+#include <LiquidCrystal_I2C.h> // Inilagay ang LiquidCrystal_I2C library para sa LCD display
+#include <DS1302.h> // Inilagay ang DS1302 library para sa RTC module
 
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-DS1302 rtc(2, 3, 4);
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // Nilikha ang isang instance ng LiquidCrystal_I2C class para sa LCD, na konektado sa address na 0x3F, at may 16 characters at 2 rows
+DS1302 rtc(2, 3, 4); // Nilikha ang isang instance ng DS1302 class para sa RTC, na konektado sa pins 2 (CLK), 3 (DAT), at 4 (RST)
 
-int pset = 8; // pushbutton for setting alarm
-int phour = 9; // pushbutton for hour
-int pmin = 10; // pushbutton for minutes
-int pexit = 11; // pushbutton for exit 
-int buzzer = 6; 
+int pset = 8; // Pin para sa pag-set ng alarm
+int phour = 9; // Pin para sa oras
+int pmin = 10; // Pin para sa minuto
+int pexit = 11; // Pin para sa pag-exit
+int buzzer = 6; // Pin para sa buzzer
 int Hour;
 int Min;
-int h = 0;
-int m = 0;
-int activate = 0;
-bool alarmActive = false;
+int h = 0; // Initial value ng oras sa alarm
+int m = 0; // Initial value ng minuto sa alarm
+int activate = 0; // Flag para sa pag-activate o pag-deactivate ng pag-set ng alarm
+bool alarmActive = false; // Flag para sa pag-activate o pag-deactivate ng alarm
 
 void setup() {
   pinMode(pset, INPUT);
   pinMode(phour, INPUT);
   pinMode(pmin, INPUT);
   pinMode(pexit, INPUT);
-  pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT); // Itinakda ang mga pin bilang input o output
   
-  lcd.begin();
-  rtc.halt(false);
-  rtc.writeProtect(false);
+  lcd.begin(); // Inisyalisasyon ng LCD
+  rtc.halt(false); // Binuksan ang RTC
+  rtc.writeProtect(false); // Tinanggal ang write protection ng RTC
 
-  // Uncomment below if you want to set initial time and date
-  // rtc.setTime(12, 0, 0); // Set the time to 12:00:00
-  // rtc.setDate(11, 11, 2024); // Set the date to November 11th, 2024
+  // Kung nais mong itakda ang oras at petsa sa simula, i-uncomment ang mga sumusunod:
+  // rtc.setTime(12, 0, 0); // Itakda ang oras sa 12:00:00
+  // rtc.setDate(11, 11, 2024); // Itakda ang petsa sa November 11, 2024
 }
 
 void loop() {
   if (!alarmActive) {
-    displayTime();
-    checkAlarmButton();
+    displayTime(); // Ipakita ang oras at petsa sa LCD
+    checkAlarmButton(); // Subukin kung pinindot ang button para sa pag-set ng alarm
   } else {
-    setAlarm();
+    setAlarm(); // Kung naka-activate ang pag-set ng alarm, i-set ito
   }
 
-  checkAlarm();
-  delay(500);
+  checkAlarm(); // Subukin kung tama na ang oras para sa alarm
+  delay(500); // Delay para sa pag-iwas sa mabilisang pag-check ng oras
 }
 
 void displayTime() {
@@ -59,9 +59,9 @@ void displayTime() {
 
 void checkAlarmButton() {
   if (digitalRead(pset) == HIGH) {
-    activate = !activate;
-    lcd.clear();
-    delay(300); // Debounce delay
+    activate = !activate; // Toggle ng flag para sa pag-set ng alarm
+    lcd.clear(); // Linisin ang LCD display
+    delay(300); // Delay para sa debounce
   }
 }
 
@@ -77,36 +77,36 @@ void setAlarm() {
   lcd.print(m);
 
   if (digitalRead(phour) == HIGH) {
-    h = (h + 1) % 24;
+    h = (h + 1) % 24; // Bawat pagpindot sa button ay magdadagdag ng oras
     lcd.setCursor(5, 1);
     lcd.print(h);
-    delay(200); // Debounce delay
+    delay(200); // Delay para sa debounce
   }
 
   if (digitalRead(pmin) == HIGH) {
-    m = (m + 1) % 60;
+    m = (m + 1) % 60; // Bawat pagpindot sa button ay magdadagdag ng minuto
     lcd.setCursor(13, 1);
     lcd.print(m);
-    delay(200); // Debounce delay
+    delay(200); // Delay para sa debounce
   }
 
   if (digitalRead(pexit) == HIGH) {
-    activate = 0;
-    lcd.clear();
-    delay(300); // Debounce delay
-    alarmActive = false;
+    activate = 0; // I-deactivate ang pag-set ng alarm
+    lcd.clear(); // Linisin ang LCD display
+    delay(300); // Delay para sa debounce
+    alarmActive = false; // I-deactivate ang alarm
   }
 }
 
 void checkAlarm() {
   if (activate && !alarmActive) {
-    DateTime now = rtc.now();
-    Hour = now.hour;
-    Min = now.minute;
+    DateTime now = rtc.now(); // Kunin ang kasalukuyang oras mula sa RTC
+    Hour = now.hour; // Kunin ang oras
+    Min = now.minute; // Kunin ang minuto
 
-    if (Hour == h && Min == m) {
-      tone(buzzer, 400, 300);
-      alarmActive = true;
+    if (Hour == h && Min == m) { // Kung ang kasalukuyang oras ay tumugma sa oras ng alarm
+      tone(buzzer, 400, 300); // Bumusina ang buzzer
+      alarmActive = true; // I-activate ang alarm
     }
   }
 }
